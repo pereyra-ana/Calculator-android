3f0c27fdbdc5125e0b4cfdc506d22a9f10f29b74
Diseño multimedial <Diseo multimedial>
2017-02-17 20:24:22 -0300

    Version que hicimos en clase

--- app/src/main/java/com/ana_pc/calculator/MainActivity.java
+++ app/src/main/java/com/ana_pc/calculator/MainActivity.java
@@ -2,10 +2,12 @@ package com.ana_pc.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,198 +17,89 @@ import java.util.logging.Logger;
 public class MainActivity extends Activity {
 
     private Integer valueOne = null;
-    private char currentOperation;
+    private String currentOperation;
     private ArrayList<String> operators = new ArrayList<>(Arrays.asList("+", "-", "*", "/"));
+    private TextView editText ;
+    private NumberOnClickListener clickNumero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = (TextView) this.findViewById(R.id.infoTextView);
+        editText = (TextView) this.findViewById(R.id.infoTextView);
 
-        final EditText editText = (EditText) this.findViewById(R.id.editText);
+        clickNumero = new NumberOnClickListener(editText);
 
         final Button buttonOne = (Button) this.findViewById(R.id.buttonOne);
-        buttonOne.setOnClickListener(new NumberOnClickListener(editText, buttonOne));
+        buttonOne.setOnClickListener(clickNumero);
 
         final Button buttonTwo = (Button) this.findViewById(R.id.buttonTwo);
-        buttonTwo.setOnClickListener(new NumberOnClickListener(editText, buttonTwo));
+        buttonTwo.setOnClickListener(clickNumero);
 
 
         final Button buttonThree = (Button) this.findViewById(R.id.buttonThree);
-        buttonThree.setOnClickListener(new NumberOnClickListener(editText, buttonThree));
+        buttonThree.setOnClickListener(clickNumero);
 
         final Button buttonFour = (Button) this.findViewById(R.id.buttonFour);
-        buttonFour.setOnClickListener(new NumberOnClickListener(editText, buttonFour));
+        buttonFour.setOnClickListener(clickNumero);
 
         final Button buttonFive = (Button) this.findViewById(R.id.buttonFive);
-        buttonFive.setOnClickListener(new NumberOnClickListener(editText, buttonFive));
+        buttonFive.setOnClickListener(clickNumero);
 
         final Button buttonSix = (Button) this.findViewById(R.id.buttonSix);
-        buttonSix.setOnClickListener(new NumberOnClickListener(editText, buttonSix));
+        buttonSix.setOnClickListener(clickNumero);
 
         final Button buttonSeven = (Button) this.findViewById(R.id.buttonSeven);
-        buttonSeven.setOnClickListener(new NumberOnClickListener(editText, buttonSeven));
+        buttonSeven.setOnClickListener(clickNumero);
 
         final Button buttonEight = (Button) this.findViewById(R.id.buttonEight);
-        buttonEight.setOnClickListener(new NumberOnClickListener(editText, buttonEight));
+        buttonEight.setOnClickListener(clickNumero);
 
         final Button buttonNine = (Button) this.findViewById(R.id.buttonNine);
-        buttonNine.setOnClickListener(new NumberOnClickListener(editText, buttonNine));
+        buttonNine.setOnClickListener(clickNumero);
 
         final Button buttonZero = (Button) this.findViewById(R.id.buttonZero);
-        buttonZero.setOnClickListener(new NumberOnClickListener(editText, buttonZero));
+        buttonZero.setOnClickListener(clickNumero);
+
+        View.OnClickListener handlerOperadores = new View.OnClickListener(){
+
+            public void onClick(View v){
+                Button operador = (Button)v;
+                if(valueOne != null){
+                    try {
+                        operarYMostrarResultado();
+                    }catch(Exception e){
+                        return;
+                    }
+                }
+                currentOperation = operador.getText().toString();
+                valueOne = getCurrentValue();
+                clickNumero.borrarTodoYMostrar = true;
+            }
+        };
 
         final Button buttonAdd = (Button) this.findViewById(R.id.buttonAdd);
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!editText.getText().toString().equals("")) {
-                    if (valueOne == null){
-                        valueOne = new Integer(editText.getText().toString());
-                    }
-                    else
-                    {
-                        if (!textView.getText().toString().isEmpty()) {
-                            valueOne = valueOne + new Integer(editText.getText().toString());
-                        }
-                        else
-                        {
-                        }
-                    }
-                    textView.setText(valueOne.toString() + buttonAdd.getText());
-                    editText.setText("");
-                    currentOperation = '+';
-                }
-            }
-        });
-
         final Button buttonSubstract = (Button) this.findViewById(R.id.buttonSubstract);
-        buttonSubstract.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!editText.getText().toString().equals("")) {
-                    if (valueOne == null) {
-                        valueOne = new Integer(editText.getText().toString());
-                    } else {
-                        if (!textView.getText().toString().isEmpty()) {
-                            valueOne = valueOne - new Integer(editText.getText().toString());
-                            textView.setText(valueOne.toString() + buttonSubstract.getText());
-                        } else {
-                            currentOperation = '-';
-                        }
-                    }
-                }
-                editText.setText("");
-            }
-        });
-
         final Button buttonMultiply = (Button) this.findViewById(R.id.buttonMultiply);
-        buttonMultiply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!editText.getText().toString().equals("")) {
-                    if (valueOne == null){
-                        valueOne = new Integer(editText.getText().toString());
-                    }
-                    else
-                    {
-                        if (!textView.getText().toString().isEmpty()) {
-                            valueOne = valueOne * new Integer(editText.getText().toString());
-                            textView.setText(valueOne.toString() + buttonMultiply.getText());
-                        }
-                        else
-                        {
-                            currentOperation = '*';
-                        }
-                    }
-                }
-                editText.setText("");
-            }
-        });
-
         final Button buttonDivide = (Button) this.findViewById(R.id.buttonDivide);
-        buttonDivide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!editText.getText().toString().equals("")) {
-                    if (valueOne == null){
-                        valueOne = new Integer(editText.getText().toString());
-                    }
-                    else
-                    {
-                        if (!textView.getText().toString().isEmpty()) {
-                            try
-                            {
-                                valueOne = valueOne / new Integer(editText.getText().toString());
-                                textView.setText(valueOne.toString() + buttonDivide.getText());
-                            }
-                            catch (Exception e)
-                            {
-                                textView.setText("Math error");
-                                valueOne = null;
-                            }
-                        }
-                        else
-                        {
-                            currentOperation = '/';
-                        }
-                    }
-                }
-                editText.setText("");
-            }
-        });
+
+
+        buttonAdd.setOnClickListener(handlerOperadores);
+        buttonSubstract.setOnClickListener(handlerOperadores);
+        buttonMultiply.setOnClickListener(handlerOperadores);
+        buttonDivide.setOnClickListener(handlerOperadores);
 
         final Button buttonEqual = (Button) this.findViewById(R.id.buttonEqual);
         buttonEqual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Logger.getLogger("app").log(Level.INFO, "edit text: '" + editText.getText().toString() + "'");
+                try {
+                    operarYMostrarResultado();
+                    Log.d("Calculadora", "Llame al igual: " + editText.getText());
+                }catch(Exception e){
 
-                if (valueOne != null && !(editText.getText().toString().equals(""))) {
-                    try {
-                        if (currentOperation == '+') {
-                            valueOne = valueOne + new Integer(editText.getText().toString());
-                        } else if (currentOperation == '-') {
-                            valueOne = valueOne - new Integer(editText.getText().toString());
-                        } else if (currentOperation == '*') {
-                            valueOne = valueOne * new Integer(editText.getText().toString());
-                        } else if (currentOperation == '/') {
-                            valueOne = valueOne / new Integer(editText.getText().toString());
-                        }
-                        textView.setText(valueOne.toString());
-                    }
-                    catch (Exception e)
-                    {
-                        textView.setText("Math error");
-                        valueOne = null;
-                    }
-                } else {
-                    if (!(textView.getText().toString().equals(""))) {
-                        if (operators.contains(textView.getText().toString().substring(textView.getText().toString().length() - 1))
-                                && editText.getText().toString().equals("")) {
-                            textView.setText("Syntax error");
-                            valueOne = null;
-                        }
-                        else {
-                            try
-                            {
-                                valueOne = new Integer(textView.getText().toString());
-                                textView.setText(valueOne.toString());
-                            }
-                            catch (Exception e)
-                            {
-                                Logger.getLogger("app").log(Level.INFO, "Not setting value because its not an integer");
-                            }
-                        }
-                    }
-                    if (!(editText.getText().toString().equals(""))) {
-                        valueOne = new Integer(editText.getText().toString());
-                        textView.setText(valueOne.toString());
-                    }
                 }
-                editText.setText("");
             }
         });
 
@@ -214,11 +107,46 @@ public class MainActivity extends Activity {
         buttonClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText(null);
-                Logger.getLogger("app").log(Level.INFO, "text view: " + textView.getText());
-                editText.setText(null);
+                editText.setText("0");
+                Log.d("Calculadora", "Aprete Borrar: " + editText.getText());
                 valueOne = null;
+                currentOperation = null;
             }
         });
+    }
+
+    private int getCurrentValue(){
+        if(this.editText.getText().toString().length() == 0){
+            return 0;
+        }else{
+            return Integer.parseInt(this.editText.getText().toString());
+        }
+    }
+
+    private void operarYMostrarResultado() throws Exception{
+        if(valueOne == null){
+            return;
+        }
+
+        int valueTwo = getCurrentValue();
+
+
+        int resultado = 0;
+
+        switch(currentOperation){
+            case "+": resultado = valueOne + valueTwo;break;
+            case "-": resultado = valueOne - valueTwo;break;
+            case "*": resultado = valueOne * valueTwo;break;
+            case "/":
+                if(valueTwo == 0){
+                    Toast.makeText(this, "No se puede dividir por cero", Toast.LENGTH_LONG).show();
+                    throw new Exception("No se puede dividir por cero");
+                }
+                resultado = valueOne / valueTwo;
+                break;
+        }
+
+        editText.setText(resultado);
+
     }
 }
--- app/src/main/java/com/ana_pc/calculator/NumberOnClickListener.java
+++ app/src/main/java/com/ana_pc/calculator/NumberOnClickListener.java
@@ -1,8 +1,10 @@
 package com.ana_pc.calculator;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Ana-pc on 16/02/2017.
@@ -11,18 +13,29 @@ import android.widget.EditText;
 public class NumberOnClickListener implements View.OnClickListener {
 
     public Button button;
-    public EditText text;
+    public TextView text;
+    public boolean borrarTodoYMostrar = true;
 
-    public NumberOnClickListener(EditText text, Button button) {
-        this.button = button;
+    public NumberOnClickListener(TextView text) {
         this.text = text;
     }
 
     @Override
     public void onClick(View v)
     {
-        this.text.setText(this.text.getText().toString() + button.getText());
+        this.button = (Button)v;
+        if(borrarTodoYMostrar){
+            this.text.setText("0");
+            borrarTodoYMostrar=false;
+        }
+        String nuevoValor = this.text.getText().toString() + button.getText();
+        this.text.setText(Integer.parseInt(nuevoValor));
+        Log.d("Calculadora", "Estoy aprendo numero: " + this.text.getText().toString());
+
+
     }
+
+
 
 
 }
